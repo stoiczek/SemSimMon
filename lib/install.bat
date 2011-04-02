7eb3930698e6013386efca8cea2466d9f341bca6
Tadeusz Kozak <stoiczek@gmail.com>
2011-04-02 13:32:21 +0200

    Added install libs script for windows

---
+++ lib/install.bat
@@ -0,0 +1,16 @@
+set PELLET_VERSION=2.0.1
+
+call mvn install:install-file -Dfile=pellet-core.jar -DgroupId=pellet -DartifactId=pellet-core -Dversion=%PELLET_VERSION% -Dpackaging=jar
+
+call mvn install:install-file -Dfile=pellet-jena.jar -DgroupId=pellet -DartifactId=pellet-jena -Dversion=%PELLET_VERSION% -Dpackaging=jar
+
+call mvn install:install-file -Dfile=pellet-rules.jar -DgroupId=pellet -DartifactId=pellet-rules -Dversion=%PELLET_VERSION% -Dpackaging=jar
+
+call mvn install:install-file -Dfile=pellet-datatypes.jar -DgroupId=pellet  -DartifactId=pellet-datatypes -Dversion=%PELLET_VERSION% -Dpackaging=jar
+
+call mvn install:install-file -Dfile=pellet-el.jar -DgroupId=pellet -DartifactId=pellet-el -Dversion=%PELLET_VERSION% -Dpackaging=jar
+
+call mvn install:install-file -Dfile=aterm-java-1.6.jar -DgroupId=aterm-java -DartifactId=aterm-java -Dversion=1.6 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=pivot-jfree-1.5.jar -DgroupId=org.apache.pivot -DartifactId=pivot-charts-jfree -Dversion=1.5 -Dpackaging=jar
+
